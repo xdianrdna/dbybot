1fdabd1e2c1ab9cd4416fff30cab726cfbed30bc
dna <denflythu@163.com>
2023-12-16 21:49:09 +0800

    temp branch

---
+++ model/user.go
@@ -0,0 +1,38 @@
+package model
+
+// 用户 api 为开发者提供有关用户操作的接口，包括获取用户信息、获取大别野成员列表和踢出大别野用户。
+
+type color string
+
+const (
+	colorRed color = "#F47884"
+	//...
+)
+
+// 用户信息
+type Member struct {
+	MemberBasic membetBasic `json:"basic"`
+
+	RoleIdList []string   `json:"role_id_list"` // 用户加入的身份组 id 列表
+	JoinedAt   string     `json:"joined_at"`    // 用户加入时间  NOTE: 返回的是 string 类型
+	RoleList   memberRole `json:"roleList"`     // 用户已加入的身份组列表
+}
+
+// 用户基本信息
+type membetBasic struct {
+	Uid       string `json:"uid"`        // uid
+	Nickname  string `json:"nickname"`   // 昵称
+	Introduct string `json:"introduce"`  // 个性签名
+	AvatarUrl string `json:"avatar_url"` // 头像地址
+}
+
+// 身份组信息
+type memberRole struct {
+	Id        uint64   `json:"id"`          // 身份组 id
+	Name      string   `json:"name"`        // 身份组名称
+	VillaId   string   `json:"villa_id"`    // 大别野 id
+	Color     color    `json:"color"`       // 身份组颜色  todo: 参考颜色
+	RoleType  string   `json:"role_type"`   //	身份组类型
+	IsAllRoom bool     `json:"is_all_room"` //	是否选择全部房间
+	RoomIDs   []uint64 `json:"room_ids"`    //	指定的房间列表
+}
--- post.go
+++ post.go
@@ -2,39 +2,97 @@ package dbybot
 
 // 向大别野发送请求的主要逻辑
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/xdianrdna/dbybot/model"
 )
+
+const baseApi = "https://bbs-api.miyoushe.com"
+
+// 基础响应
+type baseResp struct {
+	Retcode int    `json:"retcode"`
+	Message string `json:"message"`
+}
+
+// 统一响应体
+type mhyResp struct {
+	baseResp
+	Data struct {
+		Member model.Member `json:"member"`
+	} `json:"data"`
+}
+
+// 用户信息响应
+type MemberResp struct {
+	baseResp
+	Member model.Member
+}
+
+// 处理公共的加头与请求部分
+func (bot *Bot) commonHanlder(req *http.Request, villaId uint64) ([]byte, bool) {
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("x-rpc-bot_id", bot.auth.id)
+	req.Header.Add("x-rpc-bot_secret", bot.auth.secretKey)
+	req.Header.Add("x-rpc-bot_villa_id", strconv.Itoa(int(villaId)))
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Request Error:", err)
+		return nil, false
+	}
+	defer response.Body.Close()
+
+	bodyBytes, _ := io.ReadAll(response.Body)
+
+	return bodyBytes, true
+}
 
 // 机器人向大别野 api 发送请求
 // villaId 为按官方要求加入头中的参数
 func (bot *Bot) sendMessagePostRequest(villaId uint64, json string) {
-	url := "https://bbs-api.miyoushe.com/vila/api/bot/platform/sendMessage"
+	url := baseApi + "/vila/api/bot/platform/sendMessage"
 
-	body := strings.NewReader(json)
-	request, err := http.NewRequest("POST", url, body)
+	request, err := http.NewRequest("POST", url, strings.NewReader(json))
 	if err != nil {
 		fmt.Println("Build New Request Error:", err)
 		return
 	}
 
-	request.Header.Add("Content-Type", "application/json")
-	request.Header.Add("x-rpc-bot_id", bot.auth.id)
-	request.Header.Add("x-rpc-bot_secret", bot.auth.secretKey)
-	request.Header.Add("x-rpc-bot_villa_id", strconv.Itoa(int(villaId)))
+	bot.commonHanlder(request, villaId)
+}
 
-	client := &http.Client{}
-	response, err := client.Do(request)
+// 调用官方 getMember 接口，获取用户信息
+func (bot *Bot) getMember(villaId, uid uint64) *MemberResp {
+	url := baseApi + "/vila/api/bot/platform/getMember?uid=" + strconv.Itoa(int(uid))
+
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("Request Error:", err)
-		return
+		fmt.Println("Build New Request Error:", err)
+		return nil
 	}
-	defer response.Body.Close()
 
-	bodyBytes, _ := io.ReadAll(response.Body)
-	bstring := string(bodyBytes)
-	fmt.Println(bstring)
+	b, ok := bot.commonHanlder(request, villaId)
+	if !ok {
+		fmt.Println("Build New Request Error")
+		return nil
+	}
+
+	r := &mhyResp{}
+	err = json.Unmarshal(b, r)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return &MemberResp{
+		baseResp: r.baseResp,
+		Member:   r.Data.Member,
+	}
 }
